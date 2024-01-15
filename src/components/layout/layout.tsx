a5b821a317137e5bb5b5eec80942cccca9dff7ce
Damian Tulacz <damian.tulacz@gmail.com>
2024-01-15 15:01:17 +0100

    changed all .jsx to .tsx

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,6 +1,10 @@
 import styles from "@/components/layout/layout.module.scss";
 
-export default function Layout({ children }) {
+export default function Layout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
   return (
     <div className={styles.layout}>
       {children}
