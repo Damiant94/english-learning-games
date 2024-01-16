8f37e592ac0012a4e4b6b2c5a452d401068b984a
Damian Tulacz <damian.tulacz@gmail.com>
2024-01-16 14:37:34 +0100

    changed text

--- src/components/shared/Score/Score.tsx
+++ src/components/shared/Score/Score.tsx
@@ -4,7 +4,7 @@ import styles from './Score.module.scss';
 const score = (props: {answered: "" | "correct" | "wrong", score: number}) => (
   <div className={styles.ScoreWrap}>
     <div className={props.answered === 'correct' ? styles.Correct : props.answered === 'wrong' ? styles.Wrong : ''}>
-      {props.answered === 'correct' ? 'Answer correct' : props.answered === 'wrong' ? 'Answer Wrong' : 'Waiting for answer...'}
+      {props.answered === 'correct' ? 'Answer correct' : props.answered === 'wrong' ? 'Answer Wrong' : 'Waiting for answer..'}
     </div>
     <div className={styles.Score}>
       Score: {props.score}
