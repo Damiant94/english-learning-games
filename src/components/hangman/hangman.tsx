16b265a13d4944f7fd4054cc403aaa7eaedb4aa2
Damian Tulacz <damian.tulacz@gmail.com>
2024-01-17 17:47:14 +0100

    refactored file names and code

--- src/components/hangman/hangman.tsx
+++ src/components/hangman/hangman.tsx
@@ -1,11 +1,189 @@
-import Layout from '@/components/shared/Layout/Layout';
-import HangmanMain from '@/components/hangman/HangmanMain';
+import React, { useEffect, useState } from 'react';
+import Hearts from './components/Hearts/Hearts';
+import Sentence from './components/Sentence/Sentence';
+import Letters from './components/Letters/Letters';
+import Result from './components/Result/Result';
+import Backdrop from './components/Backdrop/Backdrop';
+import Translation from './components/Translation/Translation';
+
+import Btn from '../shared/Btn/Btn';
+import Spinner from '../shared/Spinner/Spinner';
+import Header from '../shared/Header/Header';
+
+import styles from './Hangman.module.scss';
+import stylesHeart from './components/Hearts/Heart/Heart.module.scss';
+import stylesLetter from './components/Letters/Letter/Letter.module.scss';
+
+import { getDefinitionsApi, getRandomWordApi } from '@/utils/api/api';
+import Layout from '../shared/Layout/Layout';
 
 
-export default function hangman() {
+const hangman = () => {
+
+  const [sentence, setSentence] = useState<string[]>([]);
+  const [currentSentence, setCurrentSentence] = useState<string>('');
+  const [definitions, setDefinitions] = useState<any>([]);
+  const [currentDefinition, setCurrentDefinition] = useState<any>('');
+  const [lives, setLives] = useState(9);
+  const [status, setStatus] = useState('progress');
+  const [backdrop, setBackdrop] = useState(false);
+
+  const setFreshState = () => {
+    setSentence([]);
+    setCurrentSentence('');
+    setDefinitions([]);
+    setCurrentDefinition('');
+    setLives(9);
+    setStatus('progress');
+    setBackdrop(false);
+  };
+
+  const setNewSentence = (sentence: string[]) => {
+    const allLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    let currentSentence = "";
+    for (const letter of sentence) {
+      if (allLetters.includes(letter)) {
+        currentSentence += "_";
+      } else {
+        currentSentence += letter;
+      }
+    }
+    setSentence(sentence);
+    setCurrentSentence(currentSentence);
+    setNewDefinitions(sentence.join(""));
+  };
+
+  const createSentence = async () => {
+    const response = await getRandomWordApi();
+    const sentence = response.toUpperCase().split("");
+    setNewSentence(sentence);
+  };
+
+  const setNewDefinitions = async (word: string) => {
+    const definitions = await getDefinitionsApi(word);
+    setDefinitions(definitions);
+    setCurrentDefinition(definitions[0]);
+  };
+
+  useEffect(() => {
+    createSentence();
+  }, [])
+
+  const restart = () => {
+    setFreshState();
+    createSentence();
+
+    for (const button of document.querySelectorAll(`.${stylesLetter.Correct}, .${stylesLetter.Wrong}`) as NodeListOf<HTMLButtonElement>) {
+      button.className = stylesLetter.Letter;
+      button.disabled = false;
+    }
+
+    for (const heart of document.querySelectorAll(`.${stylesHeart.HeartLost}`)) {
+      heart.className = stylesHeart.Heart;
+    }
+  };
+
+  const correctLetterHandle = (clickedLetter: any) => {
+    clickedLetter.classList.add(stylesLetter.Correct);
+    let newCurrentSentence = "";
+    for (const letter of sentence) {
+      if (letter === clickedLetter.innerHTML || letter === " " || currentSentence.includes(letter)) {
+        newCurrentSentence += letter;
+      } else {
+        newCurrentSentence += "_";
+      }
+    }
+    setCurrentSentence(newCurrentSentence);
+    if (!newCurrentSentence.includes("_")) {
+      setStatus('win');
+    }
+  };
+
+  const wrongLetterHandle = (clickedLetter: any) => {
+    clickedLetter.classList.add(stylesLetter.Wrong);
+    document.querySelector(`[data-heart="${lives}"]`)!.className = stylesHeart.HeartLost;
+
+    if (lives === 1) {
+      setStatus('lose');
+    }
+    setLives(prevState => prevState - 1)
+  };
+
+  const letterClickHandler = (clickedLetter: any) => {
+    clickedLetter.disabled = true;
+    if (sentence.includes(clickedLetter.innerHTML)) {
+      correctLetterHandle(clickedLetter);
+    } else {
+      wrongLetterHandle(clickedLetter);
+    }
+  };
+
+  const showDefinitionsHandler = () => {
+    setBackdrop(true);
+  };
+
+  const hideDefinitionsHandler = () => {
+    setBackdrop(false);
+  }
+
+  const changeDefinitionHandler = () => {
+    const definitionsNumber = definitions.length;
+    const currentDefinitionIndex = definitions.indexOf(currentDefinition);
+    const newDefinitionIndex = definitionsNumber - 1 !== currentDefinitionIndex ? currentDefinitionIndex + 1 : 0;
+    setCurrentDefinition(definitions[newDefinitionIndex]);
+  };
+
+  let view = null;
+  if (status === 'progress') {
+    view = (
+      <>
+        <Sentence currentSentence={currentSentence} />
+        <Letters clickHandle={letterClickHandler} />
+        <Btn clickHandle={restart} text="Reset" />
+      </>
+    );
+  } else {
+    let message, classColor;
+    if (status === 'win') {
+      message = "You won!";
+      classColor = "Win";
+    } else if (status === 'lose') {
+      message = "You lose.";
+      classColor = "Lose";
+    } else {
+      throw new Error('status has wrong value');
+    }
+    view = (
+      <>
+        <Translation
+          definition={currentDefinition}
+          definitionsNumber={definitions ? definitions.length : null}
+          show={backdrop}
+          change={changeDefinitionHandler}
+          hide={hideDefinitionsHandler}
+          sentence={sentence.join("")} />
+        <Backdrop show={backdrop} />
+        <Result
+          message={message}
+          classColor={classColor}
+          restart={restart}
+          sentence={sentence.join("")}
+          resultClicked={showDefinitionsHandler}
+        />
+      </>
+    )
+  }
+
   return (
     <Layout>
-      <HangmanMain/>
+      <div className={styles.HangmanMain}>
+        <Header text="the hangman game" />
+        <Hearts />
+        {sentence.length > 0 ? view : <Spinner />}
+      </div>
     </Layout>
-  )
+  );
 }
+
+
+export default hangman;
--- src/components/quizWord/QuizWord.tsx
+++ src/components/quizWord/QuizWord.tsx
@@ -1,10 +1,129 @@
-import Layout from '@/components/shared/Layout/Layout';
-import QuizWordMain from './QuizWordMain';
+import React, { useEffect, useRef, useState } from 'react';
+import Header from '../shared/Header/Header';
+import Spinner from '../shared/Spinner/Spinner';
+import Score from '../shared/Score/Score';
+import Answers from '../shared/Answers/Answers';
+import Question from '../shared/Question/Question';
+import Btn from '../shared/Btn/Btn';
 
-export default function quizWord() {
+import { getDefinitionApi, getRandomWordApi } from '@/utils/api/api';
+import shuffle from '../../utils/js/shuffle';
+import Answer from '@/utils/interfaces/Answer';
+
+import styles from './QuizWord.module.scss';
+import Layout from '../shared/Layout/Layout';
+
+
+const quizWord = () => {
+
+  const [currentQuestion, setCurrentQuestion] = useState('');
+  const [randomWord, setRandomWord] = useState('');
+  const [answers, setAnswers] = useState<Answer[]>([]);
+  const [dataReady, setDataReady] = useState(false);
+  const [score, setScore] = useState(0);
+  const [answered, setAnswered] = useState<'correct' | 'wrong' | ''>('');
+
+  const hasPageBeenRendered = useRef(false);
+
+  useEffect(() => {
+    getRandomWord();
+  }, [])
+
+  const getRandomWord = async () => {
+    const randomWord = await getRandomWordApi();
+    setRandomWord(randomWord);
+  };
+
+  useEffect(() => {
+    if (randomWord) {
+      addWord(randomWord);
+    }
+  },
+    [randomWord])
+
+  const addWord = async (word: string) => {
+    const definition = await getDefinitionApi(word);
+    if (await definition) {
+      if (!currentQuestion) {
+        setCurrentQuestion(word);
+        setAnswers([{
+          answer: definition,
+          isCorrect: true
+        }]);
+      } else {
+        setAnswers(prevState => {
+          const definitions = [...prevState];
+          definitions.push({
+            answer: definition,
+            isCorrect: false
+          });
+          return definitions;
+        });
+      };
+    } else {
+      getRandomWord();
+    };
+  }
+
+  useEffect(() => {
+    if (hasPageBeenRendered.current) {
+      if (answers.length < 4) {
+        getRandomWord();
+      } else {
+        setAnswers(prevState => shuffle(prevState));
+        setDataReady(true);
+      }
+    }
+    hasPageBeenRendered.current = true;
+  },
+    [answers])
+
+  const answerClicked = (isCorrect: boolean) => {
+    if (isCorrect) {
+      setScore(prevState => prevState + 1);
+      setAnswered('correct');
+    } else {
+      setAnswered('wrong');
+    }
+  }
+
+  const getNextQuestion = () => {
+    setCurrentQuestion('');
+    setRandomWord('');
+    setAnswers([]);
+    hasPageBeenRendered.current = false;
+    setDataReady(false);
+    getRandomWord();
+    setAnswered('');
+  }
+
+  const restart = () => {
+    setCurrentQuestion('');
+    setRandomWord('');
+    setAnswers([]);
+    hasPageBeenRendered.current = false;
+    setDataReady(false);
+    getRandomWord();
+    setAnswered('');
+    setScore(0);
+  }
+
+  const view = <>
+    <Question currentQuestion={currentQuestion} />
+    <Answers answers={answers} answered={answered} answerClicked={answerClicked} />
+    <Score answered={answered} score={score} />
+    <Btn text={answered === 'correct' ? 'Next Question' : 'Restart'} clickHandle={answered === 'correct' ? getNextQuestion : restart} />
+  </>
+
   return (
     <Layout>
-      <QuizWordMain/>
+      <div className={styles.QuizWordMain}>
+        <Header text="english quiz word" />
+        {dataReady ? view : <Spinner />}
+      </div>
     </Layout>
-  )
+  );
 }
+
+
+export default quizWord;
