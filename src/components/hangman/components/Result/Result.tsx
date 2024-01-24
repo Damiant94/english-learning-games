dc28f418445a418c6fb3ef2361661d284bcc78b6
Damian Tulacz <damian.tulacz@gmail.com>
2024-01-24 17:47:38 +0100

    changed class name

--- src/components/hangman/components/Result/Result.tsx
+++ src/components/hangman/components/Result/Result.tsx
@@ -4,7 +4,7 @@ import classes from './Result.module.scss';
 const Result = (props: {message: string, sentence: string, restart: () => void, classColor: string, resultClicked: () => void}) => {
 
     return (
-        <div className={classes.Results}>
+        <div className={classes.Result}>
             <p className={classes[props.classColor]}>{props.message}</p>
             <p>Correct answer:</p> 
             <p onClick={props.resultClicked}>{props.sentence}</p>
