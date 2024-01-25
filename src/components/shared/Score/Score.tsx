fe59da927ede3cab5f3f0edb3bec3baac171e4da
Damian Tulacz <damian.tulacz@gmail.com>
2024-01-25 22:55:04 +0100

    updated to use Answered interface

--- src/components/shared/Score/Score.tsx
+++ src/components/shared/Score/Score.tsx
@@ -4,8 +4,8 @@ import Answered from '@/utils/interfaces/Answered';
 
 const Score = (props: {answered: Answered, score: number}) => (
   <div className={styles.ScoreWrap}>
-    <div className={props.answered === 'correct' ? styles.Correct : props.answered === 'wrong' ? styles.Wrong : ''}>
-      {props.answered === 'correct' ? 'Answer correct' : props.answered === 'wrong' ? 'Answer Wrong' : 'Waiting for answer..'}
+    <div className={props.answered === Answered.CORRECT ? styles.Correct : props.answered === Answered.WRONG ? styles.Wrong : ''}>
+      {props.answered === Answered.CORRECT ? 'Answer correct' : props.answered === Answered.WRONG ? 'Answer Wrong' : 'Waiting for answer..'}
     </div>
     <div className={styles.Score}>
       Score: {props.score}
